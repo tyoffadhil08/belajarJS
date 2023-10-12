7c86dcdc9234915925bf7b03f322c52738c6abdc
tyoffadhil08 <tyoffadhilhaidar@gmail.com>
2023-10-12 14:53:51 +0700

    update belajarJS

---
+++ main.js
@@ -0,0 +1,53 @@
+// console.log ("hello world")
+// alert("Notifikasi")
+// prompt("aglal cantik")
+
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding is not Easy"
+// console.log(promnet)
+
+// let promnet = "Coding is easy"
+// console.log(promnet)
+// promnet = "Coding is not easy"
+// console.log(promnet)
+
+// const Promnet = "Coding is easy"
+// console.log(Promnet)
+// promnet = "coding is not easy"
+// console.log(promnet)
+
+// let totalPoin = prompt("masukkan poin anda")
+//     if(totalPoin >100){
+//         document.write("<h1>Congrats<h1/>")
+//     }
+//     else{
+//         document.write("<h1>Thanks<h1/>")
+//     }
+
+// let x =6;
+// let y =3;
+
+// console.log(x < 10 && y>1);
+// console.log(x<10&&y<1);
+// console.log(x<10&&y<1);
+// console.log(x==5||y==5);
+// console.log(x==6||y==5);
+// console.log(!(x==y));
+
+// document.writeln(x < 10 && y>1);
+// document.writeln(x<10&&y<1);
+// document.writeln(x<10&&y<1);
+// document.writeln(x==5||y==5);
+// document.writeln(x==6||y==5);
+// document.writeln(!(x==y));
+
+let p = document.querySelector("p")
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+
+button.addEventListener('click', function(){
+    let isi = input.value
+    console.log(isi)
+    p.innerText = isi
+})
